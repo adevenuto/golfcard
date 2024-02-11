b14a5d28aea6f9b0583ab720c9475afe9cbceebc
Anthony DeVenuto <adevenuto@macbook-m1.local>
2024-02-11 17:11:30 -0600

    added endpoint and continued work on search

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +18,8 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::middleware('auth:sanctum')->group( function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    
+    Route::get('/course-search', [CourseController::class, 'search']);
 });
 
 
